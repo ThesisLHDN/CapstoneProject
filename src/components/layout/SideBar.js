38b92c9ce3d864325b9711b56ecb198652071bf6
LamHoang-01 <nhlam011019@gmail.com>
2022-11-29 11:47:53 +0700

    Fix sidebar

--- src/components/layout/SideBar.js
+++ src/components/layout/SideBar.js
@@ -46,19 +46,21 @@ function SideBar() {
 
   return (
     <div 
-      className="fixed left-0 right- overflow-auto bg-gray-200 h-screen"
-      style={{ paddingLeft: "2%", paddingRight: "2%"}}
+      className="fixed overflow-auto bg-gray-200 h-screen w-1/6"
     >
-      <div className="flex mt-5 justify-center">
+      <div className="flex mt-5 mx-6">
         <p
           style={{ backgroundImage: "linear-gradient(to bottom right, #FAFF00, #5DDED6, #00FF0A)" }} 
-          className="text-4xl font-extrabold 2xl:px-3.5 2xl:py-1 xl:px-3 xl:py-0.5  rounded text-white"
+          className="text-4xl font-extrabold px-3 py-0.5 rounded text-white"
         >F</p>
-        <p className="ml-3 2xl:text-base xl:text-sm py-3 font-semibold text-green-tx">First Scrum Project</p> 
+        <p className="ml-3 2xl:text-base xl:text-sm py-3 font-semibold text-green-tx overflow-hidden truncate">First Scrum Project</p> 
       </div>
-      <p class="2xl:mx-4 xl:mx-2 mt-5 h-px border-0 bg-gray-400 font-extrabold shadow-2xl"></p>
 
-      <div className="ml-3 mt-5">
+      <div>
+        <p className="mx-6 mt-5 h-px border-0 bg-gray-400 font-extrabold shadow-2xl"></p>
+      </div>
+
+      <div className="mx-8 mt-5">
         {links.map((item) => (
           <div key={item.name} className="mt-2 hover:text-green-tx">
             <NavLink 
@@ -71,6 +73,18 @@ function SideBar() {
             </NavLink>
           </div>
         ))}
+      </div>
+
+      <div className="w-full absolute bottom-20">
+        <p className="mx-6 h-px border-0 bg-gray-400 font-extrabold shadow-2xl"></p>
+        <div className="hover:text-green-tx cursor-pointer">
+          <div className="mt-5 ml-6 mr-14">
+            <p className="text-sm overflow-hidden truncate font-bold">Dang Nguyen's Workspace aaaaaaaaaaaaaa</p>
+          </div>
+          <div className="absolute right-6 bottom-0">
+            <SettingsOutlinedIcon style={{ fontSize: "24px", color: "#008000", paddingTop: "3px" }}/>
+          </div>
+        </div>
       </div>
     </div>    
   );
